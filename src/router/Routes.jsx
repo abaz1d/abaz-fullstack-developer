cc4a901bf6fbb27f84c40dd701a5ab7e78e14907
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-09 08:50:11 +0700

    remove  .github

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import PageHome from "@/views/PageHome.jsx";
 import NotFound from "@/views/NotFound.jsx";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes as Switch,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import ScrollTopBehaviour from "@/components/ScrollTopBehaviour";
 const Routes = () => {
-  //console.log(import.meta.env.VITE_APP_BASE_API);
   return (
     <>
-      <Router basename={import.meta.env.VITE_APP_BASE_API}>
+      <BrowserRouter basename="/abaz-fullstack-developer/">
         <ScrollTopBehaviour />
         <Switch>
           <Route path="/" element={<PageHome />} />
           <Route path="*" element={<NotFound />} />
         </Switch>
-      </Router>
+      </BrowserRouter>
     </>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/abaz-fullstack-portfolio/",
+  base: "/abaz-fullstack-developer/",
   plugins: [react()],
   resolve: {
     alias: {

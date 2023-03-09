868536588b53e150fc29e027a54186b9f14bdd48
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-09 10:58:15 +0700

    add fix build

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/abaz-fullstack-developer/",
+export default defineConfig(({ command, mode }) => ({
+  base: command === "build" ? "/abaz-fullstack-developer/" : "/",
+  publicDir: command === "build" ? false : "src/assets",
+  build: {
+    brotliSize: false,
+    manifest: false,
+    minify: mode === "development" ? false : "terser",
+    outDir: "dist",
+    sourcemap: command === "serve" ? "inline" : false,
+
+    rollupOptions: {
+      output: {
+        assetFileNames: "clientlib-site/resources/[ext]/[name][extname]",
+        chunkFileNames: "clientlib-site/resources/chunks/[name].[hash].js",
+        entryFileNames: "clientlib-site/resources/js/[name].js",
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+}));

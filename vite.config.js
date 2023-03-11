c22bf434a40f81a96cdfbf52d8edfe39341bc937
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-11 14:45:40 +0700

    add domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
   base: command === "build" ? "/abaz-fullstack-developer/" : "/",
-  // publicDir: command === "build" ? "public" : "src/assets",
   build: {
     brotliSize: false,
     manifest: false,

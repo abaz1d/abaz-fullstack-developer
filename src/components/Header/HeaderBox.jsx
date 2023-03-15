83e495c3a1fc060ad09fff52eec219b6f7ad04e5
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-15 22:07:34 +0700

    version 1.2.1

--- src/components/Header/HeaderBox.jsx
+++ src/components/Header/HeaderBox.jsx
@@ -13,11 +13,9 @@ const HeaderBox = () => {
       const header = document.querySelector("header");
       const fixNav = header.offsetTop;
       if (window.pageYOffset > fixNav) {
-        header.classList.add("navbar-fixed", "h-[70px]");
-        header.classList.remove("px-8");
+        header.classList.add("navbar-fixed");
       } else {
-        header.classList.remove("navbar-fixed", "h-[70px]");
-        header.classList.add("px-8");
+        header.classList.remove("navbar-fixed");
       }
     };
     const hamburger = document.querySelector("#hamburger-btn");
@@ -53,15 +51,15 @@ const HeaderBox = () => {
     i18n.changeLanguage(e);
   };
   return (
-    <header className="duration-900 absolute top-0 left-0 z-10 flex w-full transform items-center bg-transparent py-1 px-8 lg:py-0">
+    <header className="absolute top-0 left-0 z-10 flex w-full items-center bg-transparent">
       <div className="container">
         <div className="relative flex items-center justify-between">
-          <div
-            className="text-lg  font-bold text-primary"
-            data-aos="fade-right"
-            data-aos-duration="1500"
-          >
-            <a href="/" aria-label="nav" className="block py-6">
+          <div className="px-4" data-aos="fade-right" data-aos-duration="1500">
+            <a
+              href="/"
+              aria-label="nav"
+              className="block py-6 text-lg  font-bold text-primary"
+            >
               <img
                 src={LOGO}
                 alt="logo"
@@ -72,7 +70,7 @@ const HeaderBox = () => {
             </a>
           </div>
           <div
-            className="flex items-center "
+            className="flex items-center px-4"
             data-aos="fade-left"
             data-aos-duration="1500"
           >

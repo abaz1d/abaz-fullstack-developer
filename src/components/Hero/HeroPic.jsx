e89579fc1718c7ca487d9ffea260abf4abcb0bb5
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-16 18:16:04 +0700

    version 1.2.2

--- src/components/Hero/HeroPic.jsx
+++ src/components/Hero/HeroPic.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ME from "@/assets/images/aku.svg";
+import ME from "@/assets/images/me.svg";
 
 const HeroPic = () => {
   return <img src={ME} alt="me" height={304} width={461} />;

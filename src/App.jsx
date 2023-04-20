295ae9dd45a9d850d571e75b44c3afd81a7090b5
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-04-20 08:45:04 +0700

    add new recent client

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Routes from "./router/Routes.jsx";
 import React, { useEffect, lazy, Suspense } from "react";
 import AOS from "aos";
-// import "aos/dist/aos.css";
 const ScrollToTop = lazy(() => import("./components/ScrollToTop"));
 const ToggleMode = lazy(() => import("./components/ToggleMode"));
 

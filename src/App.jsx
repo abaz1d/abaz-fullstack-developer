16aa7fc300261b337abd3f3760df8771c6e35ed0
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-04-20 07:51:21 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Routes from "./router/Routes.jsx";
 import React, { useEffect, lazy, Suspense } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 const ScrollToTop = lazy(() => import("./components/ScrollToTop"));
 const ToggleMode = lazy(() => import("./components/ToggleMode"));
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/css/app.css";
+import "./assets/css/index.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n/config.js";
 
